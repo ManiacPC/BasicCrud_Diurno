6578d1dda234f24c0fa730001bf22350c1cc9ece
Enrique Palacios <enriquepalaciosc@gmail.com>
2017-10-06 11:43:18 -0300

    Actualizado CRUD: Todos los ejemplos funcionales

--- app/src/main/java/cl/inacap/basiccrud/MainActivity.java
+++ app/src/main/java/cl/inacap/basiccrud/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 import cl.inacap.basiccrud.models.*;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,19 +18,33 @@ public class MainActivity extends AppCompatActivity {
         Contacto contacto;
         contacto = new Contacto(getApplicationContext());
 
+        /* INSERTAR */
         // contacto.insertar("Piraña", 12345, "piraña@sqm.cl");
-        if(contacto.eliminar(5)) {
+
+        /* ELIMINAR */
+        /* if(contacto.eliminar(5)) {
             Toast.makeText(getBaseContext(), "Eliminación OK", Toast.LENGTH_LONG).show();
         } else {
             Toast.makeText(getBaseContext(), "No se pudo eliminar", Toast.LENGTH_LONG).show();
+        }*/
+
+        /* ACTUALIZAR */
+        //Contacto contactoActualizar = new Contacto(1, "Tulio", 111333, "ttrivino@31minutos.cl");
+
+       // contacto.actualizar(1, contactoActualizar);
+
+        /* SELECCIONAR */
+        ArrayList<Contacto> contactos = contacto.obtenerContactos();
+        for (Contacto con : contactos) {
+            String s = new StringBuilder()
+                    .append("Cod:" + String.valueOf(con.getCodContacto()))
+                    .append(", Nombre:" + con.getNombre())
+                    .append(", Fono:" + String.valueOf(con.getFono()))
+                    .append(", Email:" + con.getEmail())
+                    .toString();
+
+            Toast.makeText(getBaseContext(), s, Toast.LENGTH_LONG).show();
         }
 
-        Contacto contactoActualizar = new Contacto();
-        contactoActualizar.codContacto = 6;
-        contactoActualizar.nombre = "Pepe";
-        contactoActualizar.fono = 5432;
-        contactoActualizar.email = "pepe3@31minutos.cl";
-
-        contacto.actualizar(6, contactoActualizar);
     }
 }
--- app/src/main/java/cl/inacap/basiccrud/models/Contacto.java
+++ app/src/main/java/cl/inacap/basiccrud/models/Contacto.java
@@ -2,23 +2,42 @@ package cl.inacap.basiccrud.models;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
 
 import cl.inacap.basiccrud.helpers.DatabaseHelper;
 
 public class Contacto {
     private DatabaseHelper helper; // Obtener el helper
 
-    public int codContacto;
-    public String nombre;
-    public int fono;
-    public String email;
+    private int codContacto;
+    private String nombre;
+    private int fono;
+    private String email;
 
+    /* Constructor a utilizar cuando se desean realizar
+     * operaciones CRUD en la base de datos con
+     * un objeto instanciado
+     */
     public Contacto(Context context) {
         // Instanciar el helper en el contexto
         this.helper = new DatabaseHelper(context);
     }
 
+    /* Constructo a utilizar cuando se desea trabajar
+     * con un objeto que almacene la información
+     * básica del modelo de contacto
+     */
+    public Contacto(int codContacto, String nombre, int fono, String email) {
+        this.codContacto = codContacto;
+        this.nombre = nombre;
+        this.fono = fono;
+        this.email = email;
+    }
+
+    /* Constructo vacío para casos específicos */
     public Contacto() { }
 
     public boolean insertar(String nombre, int fono, String email)
@@ -49,16 +68,80 @@ public class Contacto {
         SQLiteDatabase db = helper.getWritableDatabase(); // Asignamos BD
         int filasAfectadas; // affectedRows
         //                          DELETE FROM contacto WHERE codcontacto = ?
+        try {
         filasAfectadas = db.delete("contacto", "codcontacto = ?", new String[]{ String.valueOf(codcontacto) });
-        db.close();
-
         // if(filasAfectadas > 0) return true; else return false;
-        return ((filasAfectadas > 0) ? true : false); // operador ternario
+        return ((filasAfectadas > 0) ? true : false);
+        } catch (Exception e) { return false; }
+        finally { db.close(); }
     }
 
     public boolean actualizar(int codContacto, Contacto contacto){
         SQLiteDatabase db = helper.getWritableDatabase(); // Asignamos BD
+        ContentValues c = new ContentValues();
+        //     PARAMETRO    VALOR
+        c.put("NOMBRE", contacto.getNombre());
+        c.put("FONO", contacto.getFono());
+        c.put("EMAIL", contacto.getEmail());
+        try {
+            int filasAfectadas = db.update("contacto", c, "codContacto = ?", new String[]{ String.valueOf(codContacto) });
+            // if(filasAfectadas > 0) return true; else return false;
+            return ((filasAfectadas > 0) ? true : false);
+        } catch (Exception e) { return false; }
+        finally { db.close(); }
+    }
 
-        return false; // Temporal
+    public ArrayList<Contacto> obtenerContactos() {
+        ArrayList<Contacto> contactos = new ArrayList<>();
+        SQLiteDatabase db = helper.getReadableDatabase(); // Asignamos BD
+
+        // Cursor consulta = db.rawQuery("SELECT * FROM contacto WHERE codContacto = ?", new String[]{ String.valueOf(4)});
+        Cursor consulta = db.rawQuery("SELECT * FROM contacto", null);
+        if(consulta.moveToFirst()) {
+            do {
+                int codContacto = consulta.getInt(0);
+                String nombre = consulta.getString(1);
+                int fono = consulta.getInt(2);
+                String email = consulta.getString(3);
+
+                Contacto contacto = new Contacto(codContacto, nombre, fono, email);
+                contactos.add(contacto);
+            } while (consulta.moveToNext());
+            return contactos;
+        } else {
+            return null;
+        }
+    }
+
+    public int getCodContacto() {
+        return codContacto;
+    }
+
+    public void setCodContacto(int codContacto) {
+        this.codContacto = codContacto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getFono() {
+        return fono;
+    }
+
+    public void setFono(int fono) {
+        this.fono = fono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
